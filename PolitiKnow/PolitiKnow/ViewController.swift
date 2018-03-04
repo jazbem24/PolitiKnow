b693274bc9b52ba5cfd4aa93bf95d1ebc8f6b385
Jazmin Bembry <jbembry15@mail.wou.edu>
2018-03-04 11:49:52 -0800

    project started

---
+++ PolitiKnow/PolitiKnow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PolitiKnow
+//
+//  Created by Jazmin Bembry on 3/4/18.
+//  Copyright © 2018 Jazmin Bembry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
